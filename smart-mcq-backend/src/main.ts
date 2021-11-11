b0b8bd96844121063b8f6a53b8290355151556b6
BinaryMusaib <hotmusaib@gmail.com>
2021-11-11 13:52:12 +0530

    Resolved #7

--- smart-mcq-backend/src/main.ts
+++ smart-mcq-backend/src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 
+declare const module: any;
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe({
@@ -10,6 +12,10 @@ async function bootstrap() {
         disableErrorMessages: false,
         exceptionFactory: (errors) => new BadRequestException(errors),
     }));
+    if (module.hot) {
+        module.hot.accept();
+        module.hot.dispose(() => app.close());
+      }
     app.setGlobalPrefix("api");
     const prismaService: PrismaService = app.get(PrismaService);
     prismaService.enableShutdownHooks(app);

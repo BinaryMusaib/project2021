2c9ac936ef8f92b272195beb92c2c3140d58d5cb
BinaryMusaib <hotmusaib@gmail.com>
2021-11-13 19:42:25 +0530

    Edited Image alt message

--- smart-mcq-frontend/src/Home.tsx
+++ smart-mcq-frontend/src/Home.tsx
@@ -125,7 +125,7 @@ export default function ButtonAppBar() {
       </Card>
       </div>
       <div className = "designImage">
-        <img src={designImage} alt="Smart MCQs" />
+        <img src={designImage} alt="Design Image" />
         </div>
     </Box>
   );

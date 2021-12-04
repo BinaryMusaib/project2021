697c040c4bf27e9ada872c58b635a580d2571c2e
BinaryMusaib <hotmusaib@gmail.com>
2021-12-04 22:14:53 +0530

    Added Imports

--- smart-mcq-backend/src/exam/exam.module.ts
+++ smart-mcq-backend/src/exam/exam.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ExamineeListService } from './examinee-list/examinee-list.service';
 import { ExamineeListController } from './examinee-list/examinee-list.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [ExamineeListService],
   controllers: [ExamineeListController]
 })
